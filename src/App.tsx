ed162460eaf204ea5f46352864270d5ea49997ed
manav1913 <69932908+manav1913@users.noreply.github.com>
2026-04-21 19:16:57 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react'
+import axios, { Axios } from 'axios'
+import './App.css'
+import Input from './components/Input'
+
+const App = () => {
+
+
+  return (
+    <div>
+      <Input/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Input.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Input = () => {
+
+
+
+
+  return (
+    <div>
+      <form>
+
+        <input type="text" placeholder='Search City' />
+        <button>Search</button>
+
+      </form>
+    </div>
+  )
+}
+
+export default Input
